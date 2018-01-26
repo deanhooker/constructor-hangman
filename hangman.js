1efde40abad3b07d99692157b1c3b685bebcccac
Dean Hooker <deanhooker91@gmail.com>
2018-01-25 18:17:37 -0700

    variables added

--- hangman.js
+++ hangman.js
@@ -1,1 +1,22 @@
+//declare variables
 const inquirer = require('inquirer');
+
+let moviesArray = ['moonlight', 'spotlight', 'birdman', 'argo', 'juno', 'chicago', 'aliens', 'rocky', 'jaws', 'gladiator', 'shrek', 'avatar', 'up', 'inception', 'lincoln', 'interstellar', 'zootopia', 'room', 'cinderella', 'moana', 'superbad', 'fargo', 'aladdin'];
+let guessesLeft; //will be set to 10
+
+//choose word for user to guess
+
+// generate underscores
+
+// prompt user guess
+
+// if guess is correct replace underscore with letter
+// if wrong no. of guesses remaining --
+
+// prompt next user guess until all letters guessed
+// display word and congratulatory message
+// next word
+
+// or
+
+// if guesses remaining = 0 prompt user if they would like to end game
